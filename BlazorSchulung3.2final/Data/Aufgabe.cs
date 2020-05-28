d0c9ec9aaf19be05d87cfe814d684f1593d558d8
Hannes Preishuber <hannesp@ppedv.de>
2020-05-28 15:20:53 +0200

    Blazor Todo Lab

---
+++ BlazorSchulung3.2final/Data/Aufgabe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSchulung3._2final.Data
+{
+    public class Aufgabe
+    {
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public bool Erledigt { get; set; }
+        public bool IsEdit { get; set; }
+    }
+}
