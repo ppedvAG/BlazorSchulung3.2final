93920e4e1656fecb1df6eddcba18f40d25de06b6
Hannes Preishuber <hannesp@ppedv.de>
2020-05-28 10:50:30 +0200

    Partial Codebehind

---
+++ BlazorSchulung3.2final/Pages/CodeBehind.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSchulung3._2final.Pages
+{
+    public partial class CodeBehind
+    {
+
+    }
+}
