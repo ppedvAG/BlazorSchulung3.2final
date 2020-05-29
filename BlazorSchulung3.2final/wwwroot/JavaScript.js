b14450f7ac3084bea935b92c877956445a058082
Hannes Preishuber <hannesp@ppedv.de>
2020-05-29 15:47:26 +0200

    Javascript interop

---
+++ BlazorSchulung3.2final/wwwroot/JavaScript.js
@@ -0,0 +1,5 @@
+﻿window.ppedv = {
+    SetTitle: function (title) {
+        document.title = title;
+    }
+};
--- RazorClassLibrary2/ExampleJsInterop.cs
+++ RazorClassLibrary2/ExampleJsInterop.cs
@@ -7,6 +7,7 @@ namespace RazorClassLibrary2
     {
         public static ValueTask<string> Prompt(IJSRuntime jsRuntime, string message)
         {
+           
             // Implemented in exampleJsInterop.js
             return jsRuntime.InvokeAsync<string>(
                 "exampleJsFunctions.showPrompt",
