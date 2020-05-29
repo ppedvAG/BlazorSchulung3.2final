f133d0b54cc4e3c1806796cbdf512dbccd8315ae
Hannes Preishuber <hannesp@ppedv.de>
2020-05-29 15:18:00 +0200

    Component Rendertree

---
+++ BlazorSchulung3.2final/Shared/RenderTitle.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSchulung3._2final.Shared
+{
+    public class RenderTitle : ComponentBase
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(1, "title");
+            builder.AddContent(2, "mein titel");
+            builder.CloseElement();
+        }
+    }
+}
